5ceacc04d0f49d430fa81960d450d2658155ce22
MiKozzzz <milosz.kozyra@o2.pl>
2025-06-23 23:41:11 +0200

    Działa backend. Zwraca odpowiedz do frontendu

--- backend/main.py
+++ backend/main.py
@@ -4,26 +4,24 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Tutaj konfigurujesz CORS:
 origins = [
-    "http://localhost:3000",  # adres Twojego frontendowego serwera React
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # dozwolone adresy frontendów
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],        # zezwól na wszystkie metody (GET, POST itd.)
-    allow_headers=["*"],        # zezwól na wszystkie nagłówki
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Model wejściowy
 class PodrozInput(BaseModel):
     id_podrozy: str
 
-# Endpoint
 @app.post("/wybierz-podroz")
 async def wybierz_podroz(data: PodrozInput):
     print(f"Wybrano podróż: {data.id_podrozy}")
     return {"status": "ok"}
+

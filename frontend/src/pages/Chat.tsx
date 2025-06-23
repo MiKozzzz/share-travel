9b09ffeedc844a6a7e3867fdc40a4eea77fce446
MiKozzzz <milosz.kozyra@o2.pl>
2025-06-23 15:28:13 +0200

    Zmiana tekstu

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -9,7 +9,7 @@ const Chat: React.FC = () => {
             <div className="h-2 w-4 border-l-2 border-r-2 border-b-2 border-white rotate-180"></div>
           </div>
         </div>
-        Następna zaplanowana wspólna podróż: dzisiaj, 8:30, ul. Sienkiewicza 12!
+        Następna zaplanowana wspólna podróż: dzisiaj, 7:30, ul. Sienkiewicza 12!
       </div>
 
       <div className="flex-1 flex">

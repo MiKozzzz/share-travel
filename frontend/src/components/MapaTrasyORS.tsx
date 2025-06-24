3f905bd658597da17892a008b80f3dddc1d34e5b
MiKozzzz <milosz.kozyra@o2.pl>
2025-06-24 07:06:53 +0200

    Dodanie Nazw przystanków na mapie

--- frontend/src/components/MapaTrasyORS.tsx
+++ frontend/src/components/MapaTrasyORS.tsx
@@ -76,7 +76,7 @@ export default function MapaTrasyORS({ punkty_trasy, przystanki }: MapaTrasyORSP
 
       {przystanki.map((p, idx) => (
         <Marker key={idx} position={[p.lng, p.lat]}>
-          <Popup>{p.nazwa}</Popup>
+          <Popup>{idx} {p.nazwa}</Popup>
         </Marker>
       ))}
     </MapContainer>
